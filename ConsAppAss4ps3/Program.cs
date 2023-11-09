6af3846a61017856964fd1578f99eb408e0b1fa6
Rojashree1807 <rojarojashree1807@gmail.com>
2023-11-09 15:14:57 +0530

    after created

---
+++ ConsAppAss4ps3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppAss4ps3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("First 10 Numbers in Fibonacci Sequence:");
+            int n = 10;
+            int[] fibonacci = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (i <= 1)
+                {
+                    fibonacci[i] = i;
+                }
+                else
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+
+                Console.Write($"{fibonacci[i]} ");
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
